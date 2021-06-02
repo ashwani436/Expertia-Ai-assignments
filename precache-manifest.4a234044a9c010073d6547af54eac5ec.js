8ea655a29d8e5b01c46e37cb24a39787082bfc09
ashwani436 <sharmagpl786@gmail.com>
2021-06-02 20:42:06 +0530

    Updates

---
+++ precache-manifest.4a234044a9c010073d6547af54eac5ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf9b56cfcabb9551581fabdcdf1e4973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e828ecd2bc31a9bd2b5",
+    "url": "/static/css/2.61f1384f.chunk.css"
+  },
+  {
+    "revision": "50c2bb038d1520add724",
+    "url": "/static/css/main.9f684248.chunk.css"
+  },
+  {
+    "revision": "4e828ecd2bc31a9bd2b5",
+    "url": "/static/js/2.8bb67146.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/static/js/2.8bb67146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50c2bb038d1520add724",
+    "url": "/static/js/main.0544485a.chunk.js"
+  },
+  {
+    "revision": "f00d205e72c9a2c823fc",
+    "url": "/static/js/runtime-main.9964cbfa.js"
+  },
+  {
+    "revision": "70bda64236e4b8c1fbd7cf790284cb88",
+    "url": "/static/media/section2.70bda642.jpg"
+  }
+]);
